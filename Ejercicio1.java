e716cae4dd18b33744527e463ad49bb798f1374d
ItsLu1s <125322085+ItsLu1s@users.noreply.github.com>
2023-07-02 19:10:06 -0500

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int var1 = 21;
+        long var2 = 14903;
+        double var3 = 14.44;
+        boolean var4 = true;
+        String var5 = "HolaMundo";
+
+        System.out.println("Variable tipo int: " + var1);
+        System.out.println("Variable tipo long: " + var2);
+        System.out.println("Variable tipo double: " + var3);
+        System.out.println("Variable tipo boolean: " + var4);
+        System.out.println("Variable tipo String: " + var5);
+
+    }
+}
